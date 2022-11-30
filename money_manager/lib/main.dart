e14bcefa6cae05f36a0e9d14fdb34be660a82950
Yasiru\Yasiru <hly.priyadarshana@gmail.com>
2022-11-30 13:14:33 +0530

    money manager init

---
+++ money_manager/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:money_manager/pages/home.dart';
+import 'package:money_manager/shared/adaptive_theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Money Manager',
+      theme: getAdaptiveThemeData(context),
+      routes: {
+        '/': (BuildContext context) => const HomePage(),
+      },
+      onUnknownRoute: (RouteSettings settings) {
+        return MaterialPageRoute(
+            builder: (BuildContext context) => const HomePage());
+      },
+    );
+  }
+}
---
+++ money_manager/lib/shared/adaptive_theme.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+
+final ThemeData _androidTheme = ThemeData(
+  primarySwatch: Colors.orange,
+  brightness: Brightness.light,
+  indicatorColor: Colors.grey,
+  primaryColor: Colors.blueAccent,
+  buttonTheme: const ButtonThemeData(
+    buttonColor: Colors.deepPurple,
+    textTheme: ButtonTextTheme.primary,
+  ),
+);
+
+final ThemeData _iOSTheme = ThemeData(
+  brightness: Brightness.light,
+  primarySwatch: Colors.grey,
+  primaryColor: Colors.blueAccent,
+  buttonTheme: const ButtonThemeData(
+    buttonColor: Colors.deepPurple,
+    textTheme: ButtonTextTheme.primary,
+  ),
+);
+
+ThemeData getAdaptiveThemeData(context) {
+  return Platform.isAndroid ? _androidTheme : _iOSTheme;
+}
