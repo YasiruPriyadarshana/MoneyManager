42ae7ca6914629cca5d9f9cee933f1677724e6be
Yasiru\Yasiru <hly.priyadarshana@gmail.com>
2022-12-04 11:37:19 +0530

    calendar toggle option

--- money_manager/lib/pages/home.dart
+++ money_manager/lib/pages/home.dart
@@ -10,13 +10,43 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Widget buildCalander = const SizedBox();
+  bool showCalander = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Money Manager'),
       ),
-      body: const Text('content'),
+      body: Column(
+        children: [
+          TextButton(
+            onPressed: () {
+              setState(() {
+                showCalander
+                    ? buildCalander = const SizedBox()
+                    : buildCalander = const Text('this is it');
+              });
+
+              showCalander = !showCalander;
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text('Wed, Nov 2022', style: TextStyle(letterSpacing: 5.0)),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Icon(Icons.arrow_downward_rounded)
+              ],
+            ),
+          ),
+          const Center(
+            child: Text('sadasd'),
+          ),
+          buildCalander
+        ],
+      ),
     );
   }
 }
